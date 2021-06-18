c35b2e6506a03145f2531b91245178e4d09e5471
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-06-18 18:51:47 +0800

    Update problem27.py

--- Easy/problem27.py
+++ Easy/problem27.py
@@ -1,17 +1,10 @@
 # Leetcode -> Easy
 # 27. Remove Element
-# int[] nums = [...]; // Input array
-# int val = ...; // Value to remove
-# int[] expectedNums = [...]; // The expected answer with correct length.
-#                             // It is sorted with no values equaling val.
-
-# int k = removeElement(nums, val); // Calls your implementation
-
-# assert k == expectedNums.length;
-# sort(nums, 0, k); // Sort the first k elements of nums
-# for (int i = 0; i < actualLength; i++) {
-#     assert nums[i] == expectedNums[i];
-# }
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+# Note that the five elements can be returned in any order.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
 
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
