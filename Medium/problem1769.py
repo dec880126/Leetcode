28c2e8bfde95380163fc4250399b3e4d09473cd6
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-07-16 01:44:49 +0800

    Update problem1769.py

--- Medium/problem1769.py
+++ Medium/problem1769.py
@@ -8,7 +8,7 @@
 # 3) Third box: you will have to move one ball from the first box to the third box in two operations, 
 # and move one ball from the second box to the third box in one operation.
 
-# Better complement: Runtime =  68 ms ; Memory =  14.4 MB
+# Better implementation: Runtime =  68 ms ; Memory =  14.4 MB
 class Solution:
     def minOperations(self, boxes: str) -> list[int]:
         if len(boxes) == 1:
@@ -30,7 +30,7 @@ class Solution:
             out.append(tot)
         return out
 
-# Original complement: Runtime =  5360 ms ; Memory =  14.6 MB
+# Original implementation: Runtime =  5360 ms ; Memory =  14.6 MB
 # class Solution:
 #     def minOperations(self, boxes: str) -> list[int]:
 #         box_list = list(boxes)  #[0, 0, 1, 0, 1, 1]
